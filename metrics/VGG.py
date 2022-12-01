ecfc7f7bfa810d5e02b6db26a7b70f29a860dddc
Kaleb Mesfin Asfaw <kalebmes06@gmail.com>
2022-12-02 08:11:28 +0900

    add VGG loss

---
+++ metrics/VGG.py
@@ -0,0 +1,87 @@
+import torch
+import torchvision
+import torch.nn as nn
+
+class VGG19(torch.nn.Module):
+    def __init__(self, requires_grad=False):
+        super().__init__()
+        vgg_pretrained_features = torchvision.models.vgg19(pretrained=True).features
+        self.slice1 = torch.nn.Sequential()
+        self.slice2 = torch.nn.Sequential()
+        self.slice3 = torch.nn.Sequential()
+        self.slice4 = torch.nn.Sequential()
+        self.slice5 = torch.nn.Sequential()
+        for x in range(2):
+            self.slice1.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(2, 7):
+            self.slice2.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(7, 12):
+            self.slice3.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(12, 21):
+            self.slice4.add_module(str(x), vgg_pretrained_features[x])
+        for x in range(21, 30):
+            self.slice5.add_module(str(x), vgg_pretrained_features[x])
+        if not requires_grad:
+            for param in self.parameters():
+                param.requires_grad = False
+
+    def forward(self, X):
+        h_relu1 = self.slice1(X)
+        h_relu2 = self.slice2(h_relu1)
+        h_relu3 = self.slice3(h_relu2)
+        h_relu4 = self.slice4(h_relu3)
+        h_relu5 = self.slice5(h_relu4)
+        out = [h_relu1, h_relu2, h_relu3, h_relu4, h_relu5]
+        return out
+
+
+# Perceptual loss that uses a pretrained VGG network
+class VGGLoss(nn.Module):
+    def __init__(self):
+        super(VGGLoss, self).__init__()
+        self.vgg = VGG19().cuda()
+        self.criterion = nn.L1Loss()
+        self.weights = [1.0 / 32, 1.0 / 16, 1.0 / 8, 1.0 / 4, 1.0]
+
+    def forward(self, x, y):
+        x_vgg, y_vgg = self.vgg(x), self.vgg(y)
+        loss = 0
+        for i in range(len(x_vgg)):
+            loss += self.weights[i] * self.criterion(x_vgg[i], y_vgg[i].detach())
+        return loss
+    
+# You can append as many input and embedded images as you want 
+# and calculate the PSNR score using the code below
+import os
+from PIL import Image
+import torchvision.transforms as transforms
+
+input_images = []
+blended_images = []
+for im_path in os.listdir('img/input/'):
+    im_path = f'img/input/{im_path}'
+    print('analyzing: ', im_path)
+    inp_img = Image.open(im_path)
+    inp_tens = transforms.ToTensor()(inp_img).unsqueeze(0)
+    input_images.append(inp_tens[:, :3, :, :])
+
+for im_path in os.listdir('output__/'):
+    img_name = im_path
+    im_path = f'output__/{im_path}'
+    if not img_name.startswith('vis_mask') and '.' in img_name:
+        print('analyzing: ', im_path)
+        
+        bld_img2 = Image.open(im_path)
+        bld_img2 = transforms.ToTensor()(bld_img2).unsqueeze(0)
+        blended_images.append(bld_img2)
+real_imgs = torch.cat(input_images, dim=0)
+bld_imags = torch.cat(blended_images, dim=0)
+
+print(real_imgs.shape)
+print(bld_imags.shape)
+device='cuda'
+batch_size = len(real_imgs)
+print('batch_size', batch_size)
+vgg_l = VGGLoss()
+score = vgg_l(real_imgs.to(device), bld_imags.to(device))
+print(score)
